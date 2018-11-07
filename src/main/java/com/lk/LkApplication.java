18b0e5c1e5c68623fc662abc099a4642b43eb320
ikun <likun7854@163.com>
2018-11-07 15:31:14 +0800

    lk

---
+++ src/main/java/com/lk/LkApplication.java
@@ -0,0 +1,12 @@
+package com.lk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LkApplication.class, args);
+    }
+}
---
+++ src/main/java/com/lk/mapper/Demo.java
@@ -0,0 +1,17 @@
+package com.lk.mapper;
+
+/**
+ * @Auther: lk
+ * @Date: 2018/11/7 10:27
+ * @Description: 描述
+ * @Version: V1.0
+ */
+public interface Demo {
+    /**
+     *
+     * @Description: //TODO 描述
+     * @Param: 
+     * @return: 
+     *
+     */
+}
